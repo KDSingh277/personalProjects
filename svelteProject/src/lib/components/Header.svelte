4ddc494cbf7da43c9f36e5c638c87d3c83b95e77
Karan Singh <deepkaran277@gmail.com>
2024-02-22 23:29:35 -0800

    Initial setting up YT_clone

---
+++ svelteProject/src/lib/components/Header.svelte
@@ -0,0 +1,27 @@
+<header class="flex px-4 items-center h-[64px] py-2 bg-white z-10 justify-between sticky top-0 left-0" >
+    <div class="flex gap-4 items-center ">
+        <img src="/icons/bugerIcon.svg" alt="" />
+        <img src="/icons/ytLogo.svg" alt="" />
+    </div>
+
+    <div>Search Bar</div>
+
+    <div class="flex gap-3 items-center">
+        <img 
+            src="/icons/createIcon.svg" 
+            class="cursor-pointer p-3 rounded-full hover:bg-gray-200" 
+            alt=""
+        />
+        <img 
+            src="/icons/notIcon.svg" 
+            class="cursor-pointer p-3 rounded-full hover:bg-gray-200" 
+            alt=""
+        />
+        <img 
+            src="/favicon.png" 
+            class="bg-gray-200 h-8 w-8 rounded-full cursor-pointer" 
+            alt=""
+        />
+        
+    </div>
+</header>
